c936fa0a902ae38ea1ad9835db3c38201ece7c43
Hunter <hunter5000@mailinator.com>
2015-05-28 19:30:13 -0400

    Cleaned up settings.html

--- twitch-alarm/data/settingScript.js
+++ twitch-alarm/data/settingScript.js
@@ -70,6 +70,70 @@ interQual = document.getElementById("livequality")
 
 versionSpan = document.getElementById("versionspan")
 
+//Tabs
+
+        var tabLinks = new Array();
+        var contentDivs = new Array();
+
+        function init() {
+
+            var tabListItems = document.getElementById('tabs').childNodes;
+            for (var i = 0; i < tabListItems.length; i++) {
+                if (tabListItems[i].nodeName == "LI") {
+                    var tabLink = getFirstChildWithTagName(tabListItems[i], 'A');
+                    var id = getHash(tabLink.getAttribute('href'));
+                    tabLinks[id] = tabLink;
+                    contentDivs[id] = document.getElementById(id);
+                }
+            }
+
+            var i = 0;
+
+            for (var id in tabLinks) {
+                tabLinks[id].onclick = showTab;
+                tabLinks[id].onfocus = function() {
+                    this.blur()
+                };
+                if (i == 0) tabLinks[id].className = 'selected';
+                i++;
+            }
+
+            var i = 0;
+
+            for (var id in contentDivs) {
+                if (i != 0) contentDivs[id].className = 'tabContent hide';
+                i++;
+            }
+        }
+
+        function showTab() {
+            var selectedId = getHash(this.getAttribute('href'));
+
+            for (var id in contentDivs) {
+                if (id == selectedId) {
+                    tabLinks[id].className = 'selected';
+                    contentDivs[id].className = 'tabContent';
+                } else {
+                    tabLinks[id].className = '';
+                    contentDivs[id].className = 'tabContent hide';
+                }
+            }
+
+            // Stop the browser following the link
+            return false;
+        }
+
+        function getFirstChildWithTagName(element, tagName) {
+            for (var i = 0; i < element.childNodes.length; i++) {
+                if (element.childNodes[i].nodeName == tagName) return element.childNodes[i];
+            }
+        }
+
+        function getHash(url) {
+            var hashPos = url.lastIndexOf('#');
+            return url.substring(hashPos + 1);
+        }
+
 //Follower settings
 
 followClear.onclick = function() {
